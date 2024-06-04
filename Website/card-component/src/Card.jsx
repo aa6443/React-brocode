40350b31a0e5ea3b95c3ed27f945f71f9a919396
Aniket Kumar <aa6443@srmist.edu.in>
2024-06-04 21:56:09 +0530

    card components

---
+++ Website/card-component/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://w0.peakpx.com/wallpaper/349/83/HD-wallpaper-one-piece-zoro.jpg" alt="profile picture" width="300px" height="300px" />
+            <h2 className="card-title">Aniket Kumar</h2>
+            <p className="card-text">I am a 4th year student at SRM and I am learning react for fun </p>
+        </div>
+    );
+}
+
+export default Card;
